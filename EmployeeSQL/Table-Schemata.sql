2b1babaccec5bdc56bd413ba7622718a0def084b
Henry Lemen <h.lemen98@gmail.com>
2025-03-04 21:27:13 -0600

    final commit

--- EmployeeSQL/Table-Schemata.sql
+++ EmployeeSQL/Table-Schemata.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE "titles" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
+    "title_id" varchar(50)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
@@ -16,11 +16,11 @@ CREATE TABLE "salaries" (
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" varchar   NOT NULL,
+    "emp_title_id" varchar(50)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "sex" varchar   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "sex" varchar(50)   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
@@ -28,7 +28,7 @@ CREATE TABLE "employees" (
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(50)   NOT NULL,
     "emp_no" int   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
@@ -37,15 +37,15 @@ CREATE TABLE "dept_manager" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(50)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
 CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
+    "dept_no" varchar(50)   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
